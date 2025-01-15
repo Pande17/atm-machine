99b83363af99e002b2de53fde847210ce9cb18bb
Pande17 <pandedwiwjy@gmail.com>
2025-01-15 20:43:00 +0800

    add basic route

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import "github.com/gofiber/fiber/v2"
+
+func Routes(r *fiber.App) {
+	api := r.Group("/api")
+
+	api.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "TES",
+		})
+	}) 
+
+	
+}
