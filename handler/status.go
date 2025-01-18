b5709e503a362f4d34a9158485d2b68f77f57a6d
Pande17 <pandedwiwjy@gmail.com>
2025-01-19 03:04:52 +0800

    add return status preset

--- handler/status.go
+++ handler/status.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func jsonResponse(c *fiber.Ctx, statusCode int, message string, data any, deletedAt any) error {
+func jsonResponse(c *fiber.Ctx, statusCode int, message string, errLocate string, data any, deletedAt string) error {
 	response := fiber.Map{
 		"status":    statusCode,
 		"message":   message,
@@ -17,7 +17,11 @@ func jsonResponse(c *fiber.Ctx, statusCode int, message string, data any, delete
 		response["data"] = data
 	}
 
-	if deletedAt != nil {
+	if errLocate != "" {
+		response["error_location"] = errLocate
+	}
+
+	if deletedAt != "" {
 		response["deletedAt"] = deletedAt
 	}
 
@@ -25,9 +29,17 @@ func jsonResponse(c *fiber.Ctx, statusCode int, message string, data any, delete
 }
 
 func OK(c *fiber.Ctx, message string, data any) error {
-	return jsonResponse(c, fiber.StatusOK, message, data, nil)
+	return jsonResponse(c, fiber.StatusOK, message, "", data, "")
 }
 
 func BadRequest(c *fiber.Ctx, message string, errLocate string) error {
-	return jsonResponse(c, fiber.StatusBadRequest, message, nil, nil)
+	return jsonResponse(c, fiber.StatusOK, message, errLocate, nil, "")
+}
+
+func Conflict(c *fiber.Ctx, message string, errLocate string) error {
+	return jsonResponse(c, fiber.StatusConflict, message, errLocate, nil, "")
+}
+
+func Unauthorized(c *fiber.Ctx, message string, errLocate string) error {
+	return jsonResponse(c, fiber.StatusUnauthorized, message, errLocate, nil, "")
 }
