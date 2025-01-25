0a0613342c05afb34d9b8380c3f7cf64a628971b
Pande17 <pandedwiwjy@gmail.com>
2025-01-25 23:21:41 +0800

    changed name

--- model/users.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type UserAccount struct {
-	IDNumber int64   `json:"id_number" bson:"id_number"`
-	Username string  `json:"username" bson:"username"`
-	Password string  `json:"password" bson:"password"`
-	Balance  float32 `json:"balance" bson:"balance"`
-	Base     `bson:",inline"`
-}
